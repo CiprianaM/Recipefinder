da7d2d63781ad70c9377de5c92200163598a3250
CiprianaM <cipriana2001@gmail.com>
2020-02-26 19:52:19 +0100

    feat: onClick and onChange

--- src/components/SearchRecipes.js
+++ src/components/SearchRecipes.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import { Form, FormGroup, FormControl, FormLabel, Button } from 'react-bootstrap';
 
 class SearchRecipes extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ingredients: '',
+      dish: ''
+    }
+  }
   search() {
+    let {ingredients, dish} = this.state;
     // const url = 'http://www.recipepuppy.com/api/?i=apple&q=pie';
-    const url = 'http://www.recipepuppy.com/api';
+    const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;
+    console.log(this.state, url);
   }
   render () {
     return (
@@ -12,16 +21,23 @@ class SearchRecipes extends Component {
         <FormGroup>
           <FormLabel>Ingredients</FormLabel>
           {' '}
-          <FormControl type='text' placeholder='garlic, chicken' />
+          <FormControl
+            type='text'
+            placeholder='garlic, chicken'
+            onChange={event=>this.setState({ingredients: event.target.value})}  />
         </FormGroup>
         {' '}
         <FormGroup>
         <FormLabel>Dish</FormLabel>
           {' '}
-          <FormControl type='text' placeholder='adobo' />
+          <FormControl
+            type='text'
+            placeholder='adobo'
+            onChange={event=>this.setState({dish: event.target.value})}  />
         </FormGroup>
         {' '}
-        <Button>Submit</Button>
+        <Button
+          onClick={()=>this.search()}>Submit</Button>
       </Form>
     )
   }
